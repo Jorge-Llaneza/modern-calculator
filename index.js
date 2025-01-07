2243a28117d79a55e72f1bd7a937898646bffcd5
Jorge Llaneza <llanezarodriguezjorge@gmail.com>
2025-01-07 10:43:59 +0100

    reworked all the logic

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-function getButtonContent(buttonNode) {
-    p = buttonNode.querySelector("p");
-    return p.textContent
-}
-let buttons = document.querySelectorAll("button");
-buttons.forEach( (button) => {
-    addEventListener("click", manageInput(button));
-});
-manageInput(buttons) {
-    const text = getButtonContent(button);
+function manageInput(text) {
+    console.log(text)
     switch(text){
         case 1||2||3||4||5||6||7||8||9||0:
-            
+            break
 
     }
-
 }
